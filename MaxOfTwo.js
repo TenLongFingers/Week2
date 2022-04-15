12aceb27bf2ff97f9bedaab0d3a43c1e26411cd3
Chris McKnight <tenlongfingers10@gmail.com>
2022-04-14 20:42:40 -0600

    Additional Challenge

--- MaxOfTwo.js
+++ MaxOfTwo.js
@@ -1,9 +1,15 @@
-let x = 1
-let y = 2
+let x = 2
+let y = 3
 
 // Write your code below this line.
 if (x>y) {
     console.log(x)
 }
-else (y>x) ;
+if (y>x) { ;
     console.log(y)
+}
+else
+    console.log(` The values are identical.`)
+
+    //As an additional challenge, print out The values are identical. if x and y have the same value.
+
