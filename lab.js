2b7eeb1c4f8cc6db9972a5f72d03801c7b856a78
Chris McKnight <tenlongfingers10@gmail.com>
2022-04-16 13:44:24 -0600

    Problem 11 first loop before attempting to nest

--- lab.js
+++ lab.js
@@ -149,7 +149,10 @@ console.log(!changeMyMind)
 
 ////////// PROBLEM 11 //////////
 
-let z = 5;
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// let z = 5;
+// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1.
+// If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-//CODE HERE
+for(let z = 5; z > 0; z--){
+  console.log(z)
+}
