64d5608229e6128e12a9da95e185e6ef8de2f86e
Chris McKnight <tenlongfingers10@gmail.com>
2022-04-15 20:39:57 -0600

    Problem 7 works

--- lab.js
+++ lab.js
@@ -105,8 +105,9 @@ for (let i=1; i <= 10; i++) {
   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
 */
 
-//CODE HERE
-
+for (let ten=10; ten >= 0; ten--) {
+  console.log(ten)
+}
 
 ////////// PROBLEM 8 //////////
 
